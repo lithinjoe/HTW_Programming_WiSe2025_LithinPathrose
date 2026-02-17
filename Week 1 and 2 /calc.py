df5f55d77d8095ecf6b0d6870cab7efd1337e671
LithinJoePathrose <147644610+lithinjoe@users.noreply.github.com>
2026-02-17 15:43:45 +0100

    Update calc.py

--- Week 1 and 2 /calc.py
+++ Week 1 and 2 /calc.py
@@ -1,1 +1,7 @@
-
+# Task 1: Basic Math
+income = 150000
+spending = 95000
+my_profit = income - spending
+my_margin = (my_profit / income) * 100
+print("Profit:", my_profit)
+print("Margin:", my_margin)
