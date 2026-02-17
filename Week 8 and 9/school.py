30ede55f6e02fbc3e4bba81e21edbf0f547da96f
LithinJoePathrose <147644610+lithinjoe@users.noreply.github.com>
2026-02-17 15:53:27 +0100

    Update school.py

--- Week 8 and 9/school.py
+++ Week 8 and 9/school.py
@@ -1,1 +1,10 @@
-
+class Person:
+    def __init__(self, n, a):
+        self.name, self.age = n, a
+class Student(Person):
+    def __init__(self, n, a, sid):
+        super().__init__(n, a)
+        self.sid = sid
+    def info(self):
+        return f"Student: {self.name}, ID: {self.sid}"
+print(Student("Marco", 21, "12345").info())
