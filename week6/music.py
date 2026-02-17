3679ae1a85fe613fc422a93100428770ce2dd15b
LithinJoePathrose <147644610+lithinjoe@users.noreply.github.com>
2026-02-17 15:51:34 +0100

    Update music.py

--- week6/music.py
+++ week6/music.py
@@ -1,1 +1,9 @@
-
+tracks = []
+genres = {}
+# Put your own values here when running
+for i in range(3):
+    s, g = input("Song: "), input("Genre: ")
+    tracks.append((s, g))
+    genres[g] = genres.get(g, 0) + 1
+print("List:", tracks)
+print("Counts:", genres)
