38d02002c6a32ab74d88f43aa971e023323b162f
LithinJoePathrose <147644610+lithinjoe@users.noreply.github.com>
2026-02-17 15:49:32 +0100

    Update security.py

--- week4/security.py
+++ week4/security.py
@@ -1,1 +1,8 @@
-
+logs = [("user1", "success"), ("user2", "failed"), ("user2", "failed"), ("user2", "failed")]
+fails = {}
+for name, status in logs:
+    if status == "failed":
+        fails[name] = fails.get(name, 0) + 1
+for name in fails:
+    if fails[name] >= 3:
+        print("ALERT: LOCKOUT FOR", name)
